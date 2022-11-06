ba9a1ba77adbbb61694990085de51a10461abaf9
Brent <b.d.eichstadt001@gmail.com>
2022-11-06 22:49:14 +0200

    Updated main execution
    Parameterized the main function and placed the main code in a function that can be executed.

--- draw_four_points.py
+++ draw_four_points.py
@@ -13,13 +13,16 @@ import numpy as np
 import cv2
 from PIL import Image
 import doxapy
+from requests import head
 from scipy import ndimage
 from locate import this_dir
 from pathlib import Path
 
+import csv
+
 tau = 2 * np.pi
 
-
+# Used within main execution
 def mkdir_and_delete_content(path):
     path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
@@ -31,11 +34,12 @@ def mkdir_and_delete_content(path):
 
     return path
 
-
+# Used within function: get_indexes_of_a_circle_starting_from_behind
+# Used within class: Robot
 def roundi(x):
     return np.round(x).astype(int)
 
-
+# Used within main execution
 def binarize_image(filepath_from, filepath_to, upscale=1.0, **parameters):
     if "k" not in parameters:
         parameters["k"] = 0.2
@@ -58,7 +62,7 @@ def binarize_image(filepath_from, filepath_to, upscale=1.0, **parameters):
     sauvola.to_binary(binary_image, parameters)
     plt.imsave(filepath_to, binary_image, cmap="gray")
 
-
+# Used within function: get_indexes_of_all_pixels_forming_a_line
 def get_indexes_of_all_pixels_within_any_three_points(p1, p2, p3):
     """
     Get the indexes of the pixels within a trianlge of three points
@@ -87,7 +91,7 @@ def get_indexes_of_all_pixels_within_any_three_points(p1, p2, p3):
     indexes = tuple(i + x for (i, x) in zip(np.where(mask == 1), [h_min, w_min]))
     return indexes
 
-
+# Used within function: draw_slime_trail
 def get_indexes_of_all_pixels_within_any_four_points(p1, p2, p3, p4):
     """
     Get the indexes of the pixels within a quadrilateral of four points
@@ -124,7 +128,7 @@ def get_indexes_of_all_pixels_within_any_four_points(p1, p2, p3, p4):
     indexes = tuple(i + x for (i, x) in zip(np.where(mask == 1), [h_min, w_min]))
     return indexes
 
-
+# NOT USED
 def get_indexes_of_a_circle(midpoint, radius, number_of_points=100, starting_angle=0):
     """
     Get the points of a circle
@@ -146,7 +150,7 @@ def get_indexes_of_a_circle(midpoint, radius, number_of_points=100, starting_ang
         )
     return (x := np.array(points))[:, 1], x[:, 0]
 
-
+# Used in function(s): draw_on_image, get_best_angle_from_image (3 times)
 def get_indexes_of_a_circle_starting_from_behind(
     midpoint, radius, starting_angle=0, number_of_points=359
 ):
@@ -170,7 +174,7 @@ def get_indexes_of_a_circle_starting_from_behind(
         )
     return (x := roundi(np.array(points)))[:, 1], x[:, 0]
 
-
+# NOT USED
 def get_indexes_of_pixel_circle(midpoint, radius):
     """
     Get the indexes of the pixels forming a circle
@@ -198,7 +202,7 @@ def get_indexes_of_pixel_circle(midpoint, radius):
 
     return (idxes_rows[:, 1], idxes_rows[:, 2])
 
-
+# Used in function: draw_on_image
 def get_indexes_of_all_pixels_forming_a_line(p1, p2):
     """
     Get the indexes of the pixels forming a line
@@ -210,7 +214,7 @@ def get_indexes_of_all_pixels_forming_a_line(p1, p2):
     indexes = get_indexes_of_all_pixels_within_any_three_points(p1, p1, p2)
     return indexes
 
-
+# NOT USED
 def rotate_line_formed_by_two_points(p1, p2, angle):
     """
     Rotate a line formed by two points
@@ -241,7 +245,7 @@ def rotate_line_formed_by_two_points(p1, p2, angle):
     p2 = p2 + midpoint
     return p1, p2
 
-
+# NOT USED
 def get_normal_vector_of_two_points(p_left, p_right):
     """
     Get the normal vector of a line formed by two points
@@ -254,7 +258,7 @@ def get_normal_vector_of_two_points(p_left, p_right):
     normal_vector = np.array([p1_[1] - p2_[1], p2_[0] - p1_[0]])
     return normal_vector / np.linalg.norm(normal_vector)
 
-
+# Used within class: Robot
 def rotate_normal_vector(p, angle):
     """
     Rotate a normal vector
@@ -272,7 +276,7 @@ def rotate_normal_vector(p, angle):
     )
     return p
 
-
+# Used within main execution
 def add_black_border_around_image(im, border_size):
     """
     Add a black border around an image
@@ -284,7 +288,7 @@ def add_black_border_around_image(im, border_size):
     im = np.pad(im, border_size, mode="constant", constant_values=0)
     return im
 
-
+# Used in function: get_best_angle_from_image
 def blur_1d(vec, sigma=3):
     """
     Add a 1d blur to a vector
@@ -292,7 +296,7 @@ def blur_1d(vec, sigma=3):
     """
     return ndimage.gaussian_filter1d(vec, sigma)
 
-
+# Used in main function
 @dataclasses.dataclass
 class Robot:
     """
@@ -443,27 +447,29 @@ class Robot:
         if plot_dir is not None:
             plt.plot([argm-180, argm-180], [0, 1], "k")
             path = Path(plot_dir, get_current_datetime_as_string() + ".png")
+            # print(path)
             plt.savefig(path, dpi=50)
             plt.close()
             shutil.copy2(path, Path(plot_dir, "__latest__.png"))
 
         return angle, stop
 
-
+# Used in function: draw_on_image
 def draw_0_on_img(im, f, *args, **kwargs):
     indexes = f(*args, **kwargs)
     im[indexes] = 0
 
-
+# Used in function: imshow_gray_as_purple, get_best_angle_from_image
 def get_current_datetime_as_string():
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
 
-
+# Used in main execution
 def as_degree(angle):
     return angle * 360 / tau
 
 
 # Note that you should edit this further in order to export images to use in your presentation
+# Used in main execution
 def imshow_gray_as_purple(im, dir_location, *args, **kwargs):
     im = np.stack([im, im, im], axis=-1)
 
@@ -516,63 +522,176 @@ if 1:
 
     plt.imsave(Path(this_dir(), "dump.png"), np.stack([A, A, A], axis=-1))
 """
+def main_execution(xvalue, yvalue, tau_input, run, stop_range, draw_steps, rad, step_length, radius_slime, severity_slime, draw_image, from_image_location, to_image_location, plot_folder_name1, example_folder_name1, plot_folder_name2, example_folder_name2):
+    n = run
 
-if 1:
+    print('Starting on Location ' + from_image_location + ' @ x-coordinate: ' + str(xvalue) + ', y-coordinate: ' + str(yvalue) + '.')
 
-    binarize_image(
-        Path(this_dir(), "sample_image.png"),
-        Path(this_dir(), "binimage.png"),
-        upscale=4,
-        k=0.22,
-        window=75,
-    )
-    A = (plt.imread(Path(this_dir(), "binimage.png"))[:, :, 0]).astype("float")
-    A_slime = A
+    if n == 1:
 
-    # TODO: since the parameters are here, why not ask Brent put the rest of the code into a function?
-    p = SimpleNamespace(
-        start_position=(6000, 300),
-        start_angle=tau * 0.1,
-        radius=100,
-        stepsize=20,
-        slime_radius=20,
-        slime_severity=0.1,
-    )
-    plotdir = mkdir_and_delete_content(Path(this_dir(), "plots", "example"))
-    angledir = mkdir_and_delete_content(Path(this_dir(), "plots", "example_angle"))
+        binarize_image(
+            Path(this_dir(), from_image_location),
+            Path(this_dir(), to_image_location),
+            upscale=4,
+            k=0.22,
+            window=75,
+        )
+        A = (plt.imread(Path(this_dir(), to_image_location))[:, :, 0]).astype("float")
+        A_slime = A
 
-    # TODO: maybe make border radius * 1.1 or something, but that gets convoluted with the start position!
-    A = add_black_border_around_image(A, 120)
-    A_slime = add_black_border_around_image(A_slime, 120)
+        # TODO: since the parameters are here, why not ask Brent put the rest of the code into a function?
+        p = SimpleNamespace(
+            start_position=(xvalue, yvalue),
+            start_angle=tau_input * 0.1,
+            radius=rad, #p = 100
+            stepsize=step_length, #p = 20
+            slime_radius=radius_slime, #p = 20
+            slime_severity=severity_slime, #p = 0.1
+        )
+        plotdir = mkdir_and_delete_content(Path(this_dir(), plot_folder_name1, example_folder_name1))
+        angledir = mkdir_and_delete_content(Path(this_dir(), plot_folder_name2, example_folder_name2))
 
-    # Remember opencv points are (x=column,y=row) whereas numpy indexes are (i=row,j=column)
-    r = Robot(p.start_position, p.start_angle, p.radius, p.stepsize, p.slime_radius)
-    r.draw_on_image(A)
+        # TODO: maybe make border radius * 1.1 or something, but that gets convoluted with the start position!
+        A = add_black_border_around_image(A, 120)
+        A_slime = add_black_border_around_image(A_slime, 120)
 
-    r_prev = copy(r)
-    angle = 0
-    for i in range(9999999999999):
-        if i % 50 == 0:
-            imshow_gray_as_purple(A_slime, plotdir)
+        # Remember opencv points are (x=column,y=row) whereas numpy indexes are (i=row,j=column)
+        r = Robot(p.start_position, p.start_angle, p.radius, p.stepsize, p.slime_radius)
+        r.draw_on_image(A)
 
-        try:
-            r.walk(angle)
-            r.draw_on_image(A)
-            r.draw_slime_trail(A_slime, r_prev, -p.slime_severity)
-            angle, stop = r.get_best_angle_from_image(
-                A_slime,
-                #angledir
-            )
-            print(f"{int(r.angle*360/tau)}\t{int(angle*360/tau)}")
-            if stop:
+        r_prev = copy(r)
+        angle = 0
+        for i in range(stop_range):
+            if i % draw_steps == 0:
+                if draw_image == True:
+                    imshow_gray_as_purple(A_slime, plotdir)
+                else:
+                    continue
+            try:
+                r.walk(angle)
+                r.draw_on_image(A)
+                r.draw_slime_trail(A_slime, r_prev, -p.slime_severity)
+                angle, stop = r.get_best_angle_from_image(
+                    A_slime,
+                    #angledir
+                )
+                print(f"{int(r.angle*360/tau_input)}\t{int(angle*360/tau_input)}")
+                if stop:
+                    break
+                r_prev = copy(r)
+
+                # Convert A and A_slime to dataframe
+
+            # walked out of the image
+            except IndexError:
                 break
-            r_prev = copy(r)
 
-        # walked out of the image
-        except IndexError:
-            break
+        #r_prev.get_best_angle_from_image(A_slime, True)
+        #r.get_best_angle_from_image(A_slime, True)
 
-    #r_prev.get_best_angle_from_image(A_slime, True)
-    #r.get_best_angle_from_image(A_slime, True)
+        imshow_gray_as_purple(A_slime*A, plotdir)
 
-    imshow_gray_as_purple(A_slime*A, plotdir)
+        print('Ending on Location ' + from_image_location + '.')
+    else: 
+        print("Did not execute")
+
+xvalues = [550,550,2000]
+yvalues = [1150,3000,800]
+range_value = [90000,90000,90000]
+go_no_go = [1,1,1]
+steps = [1,1,1]
+v_radius = [100,100,100]
+v_step_length = [20,20,20]
+v_radius_slime = [20,20,20]
+v_severity_slime = [0.1,0.1,0.1]
+v_draw_image = [True,True,True]
+
+image_from_list = ['Meerandal_Vinyard_V1.png','Meerandal_Vinyard_V2.png','Meerandal_Vinyard_V3.png']
+image_to_list = ['binimage.png','binimage_v2.png','binimage_v3.png']
+folder_plots1 = ['plots','plots2','plots3']
+folder_example1 = ['example','example2','example3']
+angle_plots1 = ['plots','plots2','plots3']
+angle_examples = ['example_angle','example_angle2','example_angle3']
+
+print("The maximum number of images available are " + str(len(image_from_list)))
+set_image_no = int(input("Start at Image number (e.g. 1): "))
+
+image_no = 0
+
+if set_image_no > len(image_from_list) or set_image_no == None:
+    print("The number you selected is not in range. The maximum number of images available are " + str(len(image_from_list)))
+    set_image_no = int(input("Start at Image number (e.g. 1): "))
+else: 
+    image_no = set_image_no - 1
+
+image_no = set_image_no - 1
+
+while image_no <= 2:
+    main_execution(
+        xvalues[image_no],
+        yvalues[image_no],
+        tau, 
+        go_no_go[image_no], 
+        range_value[image_no], 
+        steps[image_no], 
+        v_radius[image_no], 
+        v_step_length[image_no], 
+        v_radius_slime[image_no], 
+        v_severity_slime[image_no], 
+        v_draw_image[image_no], 
+        image_from_list[image_no], 
+        image_to_list[image_no], 
+        folder_plots1[image_no], 
+        folder_example1[image_no], 
+        angle_plots1[image_no], 
+        angle_examples[image_no]
+    )
+    image_no += 1
+
+# binarize_image(
+#     Path(this_dir(), "apex.png"),
+#     Path(this_dir(), "apex_binimage.png"),
+#     upscale=4,
+#     k=0.22,
+#     window=75,
+# )
+
+# def store_bin_image_in_csv(image_name, csv_to_store):
+
+#     file = Path(this_dir(), image_name)
+#     test = np.array(Image.open(file).convert("L"))
+
+#     total_rows = len(test)
+#     # print(total_rows)
+#     total_columns = len(test[0])
+#     # print(total_columns)
+
+#     # print(len(test[1]))
+
+#     f = open(Path(this_dir(), csv_to_store), 'w', newline='')
+#     writer = csv.writer(f)
+
+#     i = 0
+#     while i <= total_rows:
+#         try:
+#             n = 0
+#             string = []
+#             while n <= total_columns:
+#                 try:
+#                     string.append(str(test[i][n]))
+#                     n += 1
+#                 except:
+#                     break
+#             writer.writerow(string)
+#             # print(string)
+#             # print('row ' + str(i) + ' completed')
+#             i += 1
+#         except:
+#             break
+
+#     f.close()
+
+# v_image_name = "sample_image.png"
+# v_csv_to_store = "binary_image_3.csv"
+
+# store_bin_image_in_csv(v_image_name,v_csv_to_store)
